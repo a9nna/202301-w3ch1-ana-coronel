d1cc694406495414505372fce78ecdb5afe8324a
Ana Coronel <a9nna.coronel@gmail.com>
2023-01-23 22:27:14 +0100

    Set interface as the option to define an object type

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,12 @@ module.exports = {
   extends: ["xo", "prettier"],
   overrides: [
     {
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+      },
       extends: ["xo-typescript", "prettier"],
       files: ["*.ts", "*.tsx"],
     },
